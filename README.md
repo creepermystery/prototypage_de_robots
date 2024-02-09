9db4a8d3b463d3d0f6d6814598cb1a4af5c7f392
creepermystery <52111693+creepermystery@users.noreply.github.com>
2024-02-09 11:37:46 +0100

    added README

---
+++ README.md
@@ -0,0 +1,21 @@
+# Robot
+## Fonctions
+Le robot doit avoir plusieurs modes de fonctionnement :
+- Suivre une ligne
+- Faire de l'évitement d'obstacles
+- Aller en ligne droite sur 1m
+- Suivre une trajectoire carrée de 50cm de côté
+- Suivre une trajectoire triangulaire de 50cm de côté
+- Suivre une trajectoire circulaire de 50cm de diamètre
+
+Tous ces programmes doivent pouvoir être lancés depuis le robot ou à distance depuis un téléphone.
+
+Chacun de ces modes change la couleur de la DEL RVB dans une couleur différente, respectivement :
+- Verte
+- Rouge
+- Blanche
+- Jaune
+- Bleue
+- Violette
+Nous avons choisi d'ajouter une couleur marron, correspondant à un erreur.
+
--- robot/robot.ino
+++ robot/robot.ino
@@ -6,26 +6,40 @@ const int PIN_DIR_MOTOR_RIGHT = 4;
 const int PIN_INTERRUPT_LEFT = 6;
 const int PIN_INTERRUPT_RIGHT = 7;
 
+// pins boutons
+const int PIN_BUTTON_VALID = 14;
+const int PIN_BUTTON_UP = 15;
+const int PIN_BUTTON_DOWN = 16;
+const int PIN_BUTTON_ON_OFF = 17;
+
 // pins DELs
 const int PIN_LED_RED = 11;
 const int PIN_LED_GREEN = 13;
 const int PIN_LED_BLUE = 12;
 
-// paramètres PWM 
+// paramètres PWM
 const int freq = 490;
 const int channel = 0;
 const int resolution = 8; // 8 bits de résolution (de 0 à 255)
 
+// variables globales
+const int rapportReductionMoteur = 53;
+const int rayonRoue = 66;
 
 int compteurDroite = 0;
 int compteurGauche = 0;
 double pi = 3.1415926535897932384626433;
 bool etatBoutonOnOff = true;
-int choix = 0;
+unsigned int choix = 0;
 
 void setup ()
 {
+	// interruptions odomètre
 	attachInterrupt(digitalPinToInterrupt(PIN_INTERRUPT_LEFT), triggerOdometreDroite(), FALLING);
+	attachInterrupt(digitalPinToInterrupt(PIN_INTERRUPT_RIGHT), triggerOdometreGauche(), FALLING);
+
+	// interruptions boutons choix
+	attachInterrupt(digitalPinToInterrupt(PIN_BUTTON_DOWN), triggerOdometreDroite(), FALLING);
 	attachInterrupt(digitalPinToInterrupt(PIN_INTERRUPT_RIGHT), triggerOdometreGauche(), FALLING);
 }
 
@@ -37,6 +51,24 @@ void triggerOdometreDroite ()
 void triggerOdometreGauche ()
 {
 	compteurGauche++;
+}
+
+void incrementerChoix ()
+{
+	choix++;
+	if (choix == 7)
+	{
+		choix = 1;
+	}
+}
+
+void decrementerChoix ()
+{
+	choix--;
+	if (choix == 0)
+	{
+		choix = 6;
+	}
 }
 
 void tournerDroite (int angle)
@@ -72,14 +104,13 @@ void tournerGauche (int angle)
 void toutDroit (int distanceCommandee)
 {
 	int distanceEstimee = 0;
-  int rayonRoue = 66;
 
 	analogWrite(PIN_MOTOR_LEFT, 255);
 	analogWrite(PIN_MOTOR_RIGHT, 255);
 
 	while (distanceEstimee < distanceCommandee)
 	{
-		distanceEstimee = pi*rayonRoue*(compteurDroite+compteurGauche);
+		distanceEstimee = pi*(rayonRoue/rapportReductionMoteur)*(compteurDroite+compteurGauche);
 	}
 
 	analogWrite(PIN_MOTOR_LEFT, 0);
@@ -90,7 +121,7 @@ void carre ()
 {
 	for (int i = 0; i = 4; i++)
 	{
-		toutDroit(10);
+		toutDroit(100);
 		tournerDroite(90);
 	}
 }
@@ -109,47 +140,79 @@ void cercle ()
 
 }
 
+void errorColor () // affiche la couleur d'erreur (marron)
+{
+	analogWrite(PIN_LED_RED, 165);
+	analogWrite(PIN_LED_GREEN, 42);
+	analogWrite(PIN_LED_BLUE, 42);
+}
+
 void loop ()
 {
 	switch (choix)
 	{
-		case 1: // suivi de ligne, del verte
+		case 1: // suivi de ligne, DEL verte
 			analogWrite(PIN_LED_RED, 0);
 			analogWrite(PIN_LED_GREEN, 255);
 			analogWrite(PIN_LED_BLUE, 0);
-			// suiviLigne();
+			if (digitalRead(PIN_BUTTON_VALID))
+			{
+				// suiviLigne();
+			}
 			break;
-		case 2: // évitement d'obstacles, del rouge
+
+		case 2: // évitement d'obstacles, DEL rouge
 			analogWrite(PIN_LED_RED, 255);
 			analogWrite(PIN_LED_GREEN, 0);
 			analogWrite(PIN_LED_BLUE, 0);
-			// evitementObstacles();
+			if (digitalRead(PIN_BUTTON_VALID))
+			{
+				// evitementObstacles();
+			}
 			break;
-		case 3: // ligne droite, del blanche
+
+		case 3: // ligne droite, DEL blanche
 			analogWrite(PIN_LED_RED, 255);
 			analogWrite(PIN_LED_GREEN, 255);
 			analogWrite(PIN_LED_BLUE, 255);
-			toutDroit(10);
+			if (digitalRead(PIN_BUTTON_VALID))
+			{
+				toutDroit(10);
+			}
 			break;
-		case 4: // carré, del jaune
+
+		case 4: // carré, DEL jaune
 			analogWrite(PIN_LED_RED, 255);
 			analogWrite(PIN_LED_GREEN, 255);
 			analogWrite(PIN_LED_BLUE, 0);
-			carre();
+			if (digitalRead(PIN_BUTTON_VALID))
+			{
+				carre();
+			}
 			break;
-		case 5: // triangle, del bleue
+
+		case 5: // triangle, DEL bleue
 			analogWrite(PIN_LED_RED, 0);
 			analogWrite(PIN_LED_GREEN, 0);
 			analogWrite(PIN_LED_BLUE, 255);
-			triangle();
+			if (digitalRead(PIN_BUTTON_VALID))
+			{
+				triangle();
+			}
 			break;
-		case 6: // cercle, del violette
-			analogWrite(PIN_LED_RED, 255);
-			analogWrite(PIN_LED_GREEN, 255);
-			analogWrite(PIN_LED_BLUE, 0);
-			cercle();
+
+		case 6: // cercle, DEL violette
+			analogWrite(PIN_LED_RED, 238);
+			analogWrite(PIN_LED_GREEN, 130);
+			analogWrite(PIN_LED_BLUE, 238);
+			if (digitalRead(PIN_BUTTON_VALID))
+			{
+				// cercle();
+			}
 			break;
-		default:
+
+		default: // n'est pas sensé arriver, affiche la couleur d'erreur
+			errorColor();
 			break;
 	}
 }
